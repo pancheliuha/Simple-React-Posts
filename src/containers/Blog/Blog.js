543e41bad10fe8389241bd88fad91a8db7e0b7b4
Victor Pancheliuha <vutya2806@gmail.com>
2018-05-10 23:19:16 +0300

    rem node_modules

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+// import axios from 'axios';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import Posts from './Posts/Posts';
+import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
+
+class Blog extends Component {
+        state = {
+            auth: true
+        };
+
+        render() {
+            return (
+                <div className="Blog">
+                    <header>
+                        <nav>
+                            <ul>
+                                <li><NavLink
+                                    to="/posts/"
+                                    exact
+                                    activeStyle={{
+                                        color: '#fa923f',
+                                        textDecoration: 'underline'
+                                    }}
+                                    activeClassName="my-active">Posts</NavLink></li>
+                                <li><NavLink to={{
+                                    pathname: '/new-post'
+                                }}>New Post</NavLink></li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <Switch>
+                        {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
+                        <Route path="/posts" component={Posts}/>
+                        <Route render={() => <h1>Not found</h1>}/>
+                        {/*<Redirect from="/" to="/posts" />*/}
+                    </Switch>
+                </div>
+            );
+        }
+    }
+
+export default Blog;
